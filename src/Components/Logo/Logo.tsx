129930fe59554758385fdae68fbb88fb30f83806
waleed <waleed.sukhon77@gmail.com>
2023-02-26 23:55:28 +0200

    logo fixed

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -1,6 +1,8 @@
 import "./style.scss";
 export default () => {
-  return <div className="logo">
-    <div></div>
-  </div>;
+  return (
+    <div className="logo">
+      <div></div>
+    </div>
+  );
 };
